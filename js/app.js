fad43fa4d630d3445f1f5dd2ec2b298edece17b5
Muhammadyusuf-Latifov <longhaochen200@gmail.com>
2025-04-17 10:21:34 +0500

    lesson

---
+++ js/app.js
@@ -0,0 +1,45 @@
+{
+    // m-1
+    let a = prompt("qoldiqli sonni kiriting")
+    console.log(Math.round(a));
+}
+    
+{
+    // m2
+let a = prompt("a sonini kiriting")
+console.log(Math.abs(a));
+}
+
+{
+    //m-4
+    let dollor = 100
+    let random = Math.random()
+    let randomNumber = Math.floor(random * 100)
+    
+   
+   
+    let result = Math.floor(dollor / randomNumber)
+    console.log(result);
+    
+}
+
+{
+    // m-5
+   
+    let a = Math.random()   
+    let random = Math.floor(a * 25)
+    let yil = 2000 
+    
+    let b = Math.random()   
+    let un = Math.floor(a * 12)
+     
+    let c = Math.random()
+    let known = Math.floor(c * 28)
+     
+    console.log(`${+yil + +random}-yil, ${un}-oy, ${known}-kun`);
+    
+
+    
+
+
+}
